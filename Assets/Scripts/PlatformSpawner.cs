da7970d1c68e698e930b7e0c2c8c043704dedc65
Michel Bourgeois <michel.bourgeois114@gmail.com>
2017-07-20 21:00:13 -0300

    Initial commit
    Unity 5 project; randomly spawning platforms

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour 
+{
+
+	float maxSpawnTimer = 2.5f;
+	float spawnTimer = 2.5f;
+
+	float speed = 7f;
+
+	float minOffset = -1f;
+	float maxOffset = 0.5f;
+
+	public GameObject platform; 
+
+	List<GameObject> currentPlatforms = new List<GameObject>();
+
+	void Start() 
+	{
+		
+	}
+
+	void Update() 
+	{
+		Move();
+		DecrementSpawnTimer();
+
+		if(spawnTimer <= 0)
+		{
+			CreatePlatform();
+			ResetSpawnTimer();
+		}
+
+		if(currentPlatforms.Count > 5)
+		{
+			DeletePlatform();
+		}
+	}
+
+	void CreatePlatform()
+	{
+		Vector3 position = new Vector3(gameObject.transform.position.x + RandomOffset(), gameObject.transform.position.y + RandomOffset(), 0);
+		GameObject newPlatform = Instantiate(platform, position, Quaternion.identity) as GameObject;
+		currentPlatforms.Add(newPlatform);
+	}
+
+	void ResetSpawnTimer()
+	{
+		spawnTimer = maxSpawnTimer;
+	}
+
+	void DecrementSpawnTimer()
+	{
+		spawnTimer -= Time.deltaTime;
+	}
+		
+
+	void DeletePlatform()
+	{
+		Destroy(currentPlatforms[0]);
+		currentPlatforms.RemoveAt(0);
+	}
+
+	float RandomOffset()
+	{
+		float offset = Random.Range(minOffset, maxOffset);
+		return offset;
+	}
+
+	void Move()
+	{
+		gameObject.transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+	float speed = 7f;
+	float jumpForce = 7f;
+
+	float jumpRayLength = 1f;
+	float sideRayLength = 1f;
+
+	int points = 0;
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		Move();
+
+		if(OnSurface())
+		{
+			Jump();
+		}
+
+		if(Falling())
+		{
+			Respawn();
+		}
+
+		if(TouchingSide())
+		{
+			Respawn();
+		}
+
+	}
+
+	void Move()
+	{
+		rb.transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+
+	void Jump()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+		}
+	}
+
+	void Respawn()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	bool Falling()
+	{
+		if(gameObject.transform.position.y <= -15)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool OnSurface()
+	{
+		if(Physics.Raycast(rb.transform.position, Vector3.down, jumpRayLength))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool TouchingSide()
+	{
+		if(Physics.Raycast(rb.transform.position, Vector3.right, sideRayLength))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	void AddPoint()
+	{
+		points += 1;
+	}
+}
